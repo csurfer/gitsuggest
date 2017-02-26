95600368bb11d825199aa5b3a234502c4b9ced15
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2017-02-26 11:40:40 -0800

    Add base template for core functionality.

---
+++ git_suggest/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#    _______ __  _____                             __
+#   / ____(_) /_/ ___/__  ______ _____ ____  _____/ /_
+#  / / __/ / __/\__ \/ / / / __ `/ __ `/ _ \/ ___/ __/
+# / /_/ / / /_ ___/ / /_/ / /_/ / /_/ /  __(__  ) /_
+# \____/_/\__//____/\__,_/\__, /\__, /\___/____/\__/
+#                        /____//____/
+
+"""
+GitSuggest
+~~~~~~~~~~
+
+GitSuggest is a library written in Python, to suggest git repositories a user
+might be interested in based on user's interests.
+
+:copyright: (c) 2017 by Vishwas B Sharma.
+:licence: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'git_suggest'
+__version__ = '0.0.1'
+__author__ = 'Vishwas B Sharma'
+__author_email__ = 'sharma.vishwas88@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Vishwas B Sharma'
+
+from .suggest import GitSuggest
---
+++ git_suggest/suggest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+git_suggest.suggest
+~~~~~~~~~~~~~~~~~~~
+
+This module contains the primary objects that power GitSuggest.
+"""
+
+class GitSuggest(object):
+    """Class to suggest git repositories for a user."""
+    pass
