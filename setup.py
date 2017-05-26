ee471bd423e657006df41a29f1a99ed042b1b841
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2017-05-25 20:38:27 -0700

    Update version to send package with keywords.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name='gitsuggest',
 
     # Details
-    version='0.0.1',
+    version='0.0.2',
     description='A tool to suggest github repositories based on the' +
                 ' repositories you have shown interest in.',
     long_description=long_description,
